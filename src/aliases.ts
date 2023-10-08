5c7b12c7492861e94787995b79350c0bb046335c
Святослав Деев <109512200+xkochevnikx@users.noreply.github.com>
2023-10-08 14:11:45 +0300

    add aliases

---
+++ src/aliases.ts
@@ -0,0 +1,13 @@
+type MyBoolean = false | true;
+
+type Pair = [string, string];
+
+type Pairs = Pair[];
+
+type DiffCar = {
+    wheels: number;
+    brand: string;
+    type: string;
+    isNew?: boolean;
+    [key: string]: unknown | string;
+};
