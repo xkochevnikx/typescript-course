a87c5ba0b39e20aa4912a55fe2f690a5156b47a7
Святослав Деев <109512200+xkochevnikx@users.noreply.github.com>
2023-10-08 13:44:42 +0300

    add tuples

---
+++ dist/tuples.js
@@ -0,0 +1,13 @@
+"use strict";
+//кортежи
+const pairs = [
+    ['key', 'value'],
+    ['key2', 'value2'],
+];
+const data = [
+    [1, true, 'string'],
+    [1, true, 'string'],
+];
+data.push([4, false, 'ee']);
+//csv
+const doc = [];
---
+++ src/tuples.ts
@@ -0,0 +1,16 @@
+//кортежи
+
+const pairs: [string, string][] = [
+    ['key', 'value'],
+    ['key2', 'value2'],
+];
+
+const data: [number, boolean, string][] = [
+    [1, true, 'string'],
+    [1, true, 'string'],
+];
+
+data.push([4, false, 'ee']);
+
+//csv
+const doc: [string, string, number, Date][] = [];
