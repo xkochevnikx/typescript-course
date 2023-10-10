09da4d517bc9adfa176ffa7a8be0d4dd2bc719da
Святослав Деев <109512200+xkochevnikx@users.noreply.github.com>
2023-10-10 11:28:14 +0300

    add narriowing

---
+++ src/narriowing.ts
@@ -0,0 +1,46 @@
+//сужение типов
+
+function exampl(x: string | number) {
+    if (typeof x === 'string') {
+        x.toLowerCase();
+    } else if (typeof x === 'number') {
+        x.toFixed();
+    } else if (x === undefined) {
+        console.log('no value');
+    }
+}
+
+//посколку null тоже объект как и массив для проверки на массив делает такую проверку
+function example(x: string | string[] | null) {
+    // if(typeof x === "object") {}
+    // if (x && typeof x === 'object') {
+    //     x.map(() => {});
+    //
+
+    if (typeof x === 'string') {
+        x.toLowerCase();
+    } else if (Array.isArray(x)) {
+        x.filter(() => {});
+    }
+}
+
+//пример проверки на инстансе объекта даты
+function exmple1(x: number[] | Date) {
+    if (x instanceof Date) {
+        x.getHours();
+    } else {
+        x.map(() => {});
+    }
+}
+
+//
+type Fish = { swim: () => void };
+type Bird = { fly: () => void };
+
+function move(animal: Fish | Bird) {
+    if ('swim' in animal) {
+        return animal.swim();
+    }
+
+    return animal.fly();
+}
