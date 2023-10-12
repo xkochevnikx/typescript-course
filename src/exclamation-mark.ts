a82d0e5a4af0ac254c0d90049d2d25f5b6750ec0
Святослав Деев <109512200+xkochevnikx@users.noreply.github.com>
2023-10-12 09:40:40 +0300

    add exclamation

---
+++ src/exclamation-mark.ts
@@ -0,0 +1,12 @@
+// символ утверждения используется в крайних случаях когда мы точно уверены что значнеие есть
+
+let word: string | null = null;
+let num2 = 5;
+if (num2 <= 5) {
+    word = 'd';
+}
+console.log(word!.toLowerCase());
+
+function printName(name?: string) {
+    const fullName: string = name!;
+}
