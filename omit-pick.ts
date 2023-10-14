6d6e8a6d018e46d5afc29f52d506fb03156c79a0
Святослав Деев <109512200+xkochevnikx@users.noreply.github.com>
2023-10-14 15:10:04 +0300

    add omit, pick

---
+++ omit-pick.ts
@@ -0,0 +1,10 @@
+//утилита omit принимает первым аргументом тип который из которого нужно выкинуть конкретное поле а pick наоборот возвращает те свойства что были переданы юнионом во втором параметре
+interface ITodo {
+    id: number;
+    title: string;
+    completed: boolean;
+}
+
+type TodoSimple = Omit<ITodo, 'completed'>;
+
+type TodoSimple2 = Pick<ITodo, 'completed'>;
